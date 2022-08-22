7e3ab406b4c67c2d7e2bcc25784561e2ab0ccae7
moiz.akhtar <moiz.akhtar@pitb.gov.pk>
2022-08-22 23:35:07 +0500

    # Configuration

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -5,7 +5,7 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
-    <title>Laravel</title>
+    <title>LTBA PORTAL</title>
 
     <!-- Fonts -->
     <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
@@ -84,7 +84,7 @@
 
         <div class="content">
             <div class="title m-b-md">
-                Welcome
+                <img src="{{asset('public/images/logo.png')}}" alt="LTBA">
             </div>
         </div>
     </div>
--- routes/web.php
+++ routes/web.php
@@ -10,29 +10,25 @@ Route::get('/', function () {
  *****************************************************************************
  ************************** ADMIN PANEL ROUTES *******************************
  *****************************************************************************
-*/
-Route::group(['middleware' => 'prevent-back-history'], function()
-{
+ */
+Route::group(['middleware' => 'prevent-back-history'], function () {
     Route::get('/dashboard', function () {
-        if(Auth::guard('admin')->check()){
+        if (Auth::guard('admin')->check()) {
             return redirect()->route('admin.dashboard');
         }
         return view('admin.login');
     });
 
-    Route::prefix('admin')->group(function()
-    {   
-        Route::match(['get','post'],'/','AdminController@login')->name('admin.login');
+    Route::prefix('portal')->group(function () {
+        Route::match(['get', 'post'], '/login', 'AdminController@login')->name('admin.login');
 
-        Route::group(['middleware' => ['adminCheckSuspend']],function()
-        {
-            Route::group(['middleware' => ['admin']],function()
-            {
+        Route::group(['middleware' => ['adminCheckSuspend']], function () {
+            Route::group(['middleware' => ['admin']], function () {
                 Route::get('/dashboard', 'AdminController@dashboard')->name('admin.dashboard');
                 Route::get('/logout', 'AdminController@logout')->name('admin.logout');
 
-                Route::group(['middleware' => ['permission:manage-admin-users']],function(){
-                    Route::group(['prefix' => 'admin-users'],function(){
+                Route::group(['middleware' => ['permission:manage-admin-users']], function () {
+                    Route::group(['prefix' => 'admin-users'], function () {
                         Route::get('/', 'AdminUserController@index')->name('admins.index');
                         Route::get('/create', 'AdminUserController@create')->name('admins.create');
                         Route::post('/store', 'AdminUserController@store')->name('admins.store');
