427e38baa8428394b10e108e5c83bba4160edbff
moiz.akhtar <moiz.akhtar@pitb.gov.pk>
2022-08-28 18:24:03 +0500

    # Election module create form correction

--- app/Http/Controllers/ElectionController.php
+++ app/Http/Controllers/ElectionController.php
@@ -3,8 +3,8 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-use Validator;
 use App\Election;
+use Illuminate\Support\Facades\Validator;
 use Yajra\DataTables\DataTables;
 
 class ElectionController extends Controller
@@ -17,28 +17,28 @@ class ElectionController extends Controller
     public function index(Request $request)
     {
         if ($request->ajax()) {
-            $data = Election::orderBy('id','DESC');
+            $data = Election::orderBy('id', 'DESC');
             return Datatables::of($data)
-            ->addIndexColumn()
-            ->addColumn('status', function (Election $data) {
-                if ($data->status == 1) {
-                    $status = '<span class="badge badge-success">Active</span>';
-                } else {
-                    $status = '<span class="badge badge-danger">Inactive</span>';
-                }
-                return $status;
-            })
-            ->addColumn('action', function (Election $data) {
-                $btn ='<a href="' . route('elections.edit', $data->id) . '" class="edit btn btn-primary btn-sm"><i class="fas fa-edit"></i> Edit </a>';
-                if ($data->status == 1) {
-                    $status = '<a onclick="changeStatus(' . $data->id . ',0)" href="javascript:void(0)" class="btn btn-sm btn-danger mt-1">Deactivate</a>';
-                } else {
-                    $status = '<a onclick="changeStatus(' . $data->id . ',1)" href="javascript:void(0)" class="btn btn-sm btn-success mt-1">Activate</a>';
-                }
-                return $btn . " " . $status;
-            })
-            ->rawColumns(['action','status'])
-            ->make(true);
+                ->addIndexColumn()
+                ->addColumn('status', function (Election $data) {
+                    if ($data->status == 1) {
+                        $status = '<span class="badge badge-success">Active</span>';
+                    } else {
+                        $status = '<span class="badge badge-danger">Inactive</span>';
+                    }
+                    return $status;
+                })
+                ->addColumn('action', function (Election $data) {
+                    $btn = '<a href="' . route('elections.edit', $data->id) . '" class="edit btn btn-primary btn-sm"><i class="fas fa-edit"></i> Edit </a>';
+                    if ($data->status == 1) {
+                        $status = '<a onclick="changeStatus(' . $data->id . ',0)" href="javascript:void(0)" class="btn btn-sm btn-danger mt-1">Deactivate</a>';
+                    } else {
+                        $status = '<a onclick="changeStatus(' . $data->id . ',1)" href="javascript:void(0)" class="btn btn-sm btn-success mt-1">Activate</a>';
+                    }
+                    return $btn . " " . $status;
+                })
+                ->rawColumns(['action', 'status'])
+                ->make(true);
         }
 
         return view('admin.elections.index');
@@ -76,15 +76,15 @@ class ElectionController extends Controller
             ], 400);
         }
 
-        $electionData = [
+        $data = [
             'name' => $request->input('name'),
             'start_date' => $request->input('start_date'),
             'end_date' => $request->input('end_date'),
         ];
 
-        $election = Election::create($electionData);
+        Election::create($data);
 
-        return response()->json([ 'status' => 1, 'message' => 'success']);
+        return response()->json(['status' => 1, 'message' => 'success']);
     }
 
     /**
@@ -110,7 +110,7 @@ class ElectionController extends Controller
         if ($election == null) {
             return redirect()->back()->with('error', 'No Record Found.');
         }
-        
+
         return view('admin.elections.edit', compact('election'));
     }
 
@@ -123,10 +123,7 @@ class ElectionController extends Controller
      */
     public function update(Request $request, $id)
     {
-        $election = Election::find($id);
-        if ($election == null) {
-            return redirect()->back()->with('error', 'No Record Found.');
-        }
+        $election = Election::findOrFail($id);
 
         $rules = [
             'name' => 'required|string|max:50|unique:elections,name,' . $election->id,
@@ -142,15 +139,15 @@ class ElectionController extends Controller
             ], 400);
         }
 
-        $electionData = [
+        $data = [
             'name' => $request->input('name'),
             'start_date' => $request->input('start_date'),
             'end_date' => $request->input('end_date'),
         ];
 
-        $election->update($electionData);
+        $election->update($data);
 
-        return response()->json([ 'status' => 1, 'message' => 'success']);
+        return response()->json(['status' => 1, 'message' => 'success']);
     }
 
     /**
