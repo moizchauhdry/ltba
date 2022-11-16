51eaa9ee4e54554aef8d5802a9cccf05bf0245ae
moiz.akhtar <moiz.akhtar@pitb.gov.pk>
2022-11-16 14:38:14 +0500

    Tasks November 11, 2022 #20

--- app/Imports/MemberImport.php
+++ app/Imports/MemberImport.php
@@ -15,9 +15,11 @@ class MemberImport implements ToModel, WithHeadingRow
      */
     public function model(array $row)
     {
+        // dd($row);
         $member = Member::updateOrCreate(['mem_no' => $row['membership']], [
             'mem_no' => $row['membership'],
             'name'  => $row['name'],
+            'father_name'  => $row['father_name'],
             'cnic_no'  => $row['cnic'],
             'mem' => $row['lifeord'],
             'membership_based_on' => $row['qly'],
@@ -25,7 +27,11 @@ class MemberImport implements ToModel, WithHeadingRow
             'contact_no' => $row['cell'],
             'gender' => $row['gender'],
             'birth_date' => $row['d_o_b'],
+            // 'fees' => $row['fees'],
             'residential_address' => $row['residential_address'],
+            'office_address' => $row['office_address'],
+            'mem_reg_date' => $row['membership_reg_date'],
+            'qualification' => $row['qualification'],
             'mem_status' => 1,
         ]);
 
