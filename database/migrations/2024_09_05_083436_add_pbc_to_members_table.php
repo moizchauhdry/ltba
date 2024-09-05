bb75bc794402fcdd8a29826cdd8b37a45b081d72
syed-husnain <asyedhusnainali@gmail.com>
2024-09-05 14:36:42 +0600

    add migration

---
+++ database/migrations/2024_09_05_083436_add_pbc_to_members_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class AddPbcToMembersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('members', function (Blueprint $table) {
+            $table->text('license_front')->nullable();
+            $table->text('license_back')->nullable();
+            $table->text('cnic_front')->nullable();
+            $table->text('cnic_front')->nullable();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('members', function (Blueprint $table) {
+            $table->dropColumn(['license_front', 'license_back', 'cnic_front', 'cnic_back']);
+        });
+    }
+}
